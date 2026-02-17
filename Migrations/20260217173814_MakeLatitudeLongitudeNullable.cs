d33ac2ed3d13c0363038f3fb5161a54a0b2a47b2
swayamkrushi <bskwater7@gmail.com>
2026-02-17 23:09:08 +0530

    changes made in orderController

---
+++ Migrations/20260217173814_MakeLatitudeLongitudeNullable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BhagiRadhaSwayamKrushi.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeLatitudeLongitudeNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "BookingDate",
+                table: "Orders",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "BookingDate",
+                table: "Orders",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+        }
+    }
+}
